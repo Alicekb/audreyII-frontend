f14e38a9472087d8733b28e0d439582c50b1c207
AliceKB <alicekb01@gmail.com@gmail.com>
2017-05-09 20:42:34 -0400

    Created static Home component.

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Home = () => (
-  <div>
-    <h1>Welcome to the Jungle!</h1>
-  </div>
-);
+import Login from './Login';
+import Signup from './Signup';
 
-export default Home;
+const homeStyle = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)'
+}
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className='ui container grid center aligned' style={homeStyle}>
+        <div className='ui divided two column row'>
+          <div className='ui column' style={{background: '#fff', padding: '2em'}}>
+            <h1>Audrey II</h1>
+            <p>Yummy java main course salt sustainable.
+              Main course grocery shopping yummy lovely fruit dish cookie
+              custard quinoa healthy pub luncheon. Salt lunch sushi
+              dinner delicious marinate bread flavor farm herbes butter.
+              Butter main course organic ingredients liquor chopsticks
+              sustainable biological sous-chef bartender wine sweet broil simmer.
+              Gluten free fruit quinoa fresh organic locally grown scent bistro tasty.
+              Recommendations gluten free blender. Wholesome authentic cuisine gastronome
+              vegetables al dente yummy cookie relish mustard fresh etiquette chopsticks.
+              Sour bar sustainable cuisine liquor bon appetit heating broil consumer
+              chocolate carrots main course.</p>
+          </div>
+
+          <div className='ui column' style={{background: '#fff', padding: '2em'}}>
+            <Signup />        
+          </div>
+        </div>
+      </div>
+   );
+  }
+}
---
+++ src/components/main/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Signin = () => (
+  <form className='ui form fluid container' style={{padding: '2em', color: '#83B692'}}>
+    <div className='field'>
+      <label>Email:</label>
+      <input placeholder='Email' />
+    </div>
+    
+    <div className='field'>
+      <label>Password</label>
+      <input type='password'/>
+    </div>
+    <button type='submit' className='ui button'>Login</button>
+     <h4><Link to='/' classname='item'>Sign-up instead</Link></h4>
+  </form>
+);
+
+export default Signin;
