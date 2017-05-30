89b00c69717b206ab24ab37f393f7d36c2f71b56
AliceKB <alicekb01@gmail.com@gmail.com>
2017-05-30 14:26:46 -0400

    Refactored GroceryList and got rid of usless components.

--- src/components/week_planner/GroceryList.js
+++ src/components/week_planner/GroceryList.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
-import IngredientList from './IngredientList';
 
-export default class GroceryList extends Component {
-  render() {
-    return (
-      <div className='row'>
-        <div className='ui divided three column stackable grid container' style={{background: '#fff'}}>
-          <h2><u>Groceries</u></h2>
-          <IngredientList />
-          <IngredientList />
-          <IngredientList />
-          <IngredientList />
-          <IngredientList />
-          <IngredientList />
+const GroceryList = () => (
+   <div className='row'>
+      <div className='ui divided three column stackable grid container' style={{background: '#fff'}}>
+        <h2><u>Groceries</u></h2>
+        <div className='column'>
+          <div className='column'>
+            <p>ingredient</p>
+          </div>
+          <div className='column'>
+            <p>ingredient</p>
+          </div>
+          <div className='column'>
+            <p>ingredient</p>
+          </div>
+          <div className='column'>
+            <p>ingredient</p>
+          </div>
         </div>
       </div>
-    );
-  }
-}
+  </div>
+)
+
+export default GroceryList
--- src/components/week_planner/Ingredient.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Ingredient = () => (
-  <div className='column'>
-    <p>ingredient</p>
-  </div>
-);
-
-export default Ingredient;
