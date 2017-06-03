a4724e90e22aab1bd051b75f72356d53f55acf07
AliceKB <alicekb01@gmail.com@gmail.com>
2017-06-03 19:50:51 -0400

    Merged Recipe component to infocard.

--- src/components/meal_planner/InfoCard.js
+++ src/components/meal_planner/InfoCard.js
@@ -8,7 +8,30 @@ const infoStyle = {
 
 const InfoCard = () => (
   <div className='row' style={infoStyle}>
-    <Recipe />
+    <div className='container' style={{paddingLeft: '.5em'}}>
+      <h3>Ingredients List</h3>
+        <ul className='ui list'>
+          <li className='item'>1 medium-size whole chicken (about 4 lbs)</li>
+          <li className='item'>Salt and pepper (about ¾ tsp kosher salt per lb)</li>
+          <li className='item'>½ cup sage pesto</li>
+        </ul>
+      <h3>Calories</h3>
+        <ul className='ui list'>
+          <li className='item'>948.5783323955536</li>
+        </ul>
+      <h3>Recipe URL</h3>
+        <ul className='ui list'>
+          <li className='item'>
+            <a 
+              href="http://www.epicurious.com/recipes/food/views/Dijon-and-Tarragon-Grilled-Chicken-105171"
+              target='_blank' 
+              rel="noopener noreferrer"
+            >
+              Sunday Supper: Jerk Half-Chickens
+            </a>
+          </li>
+        </ul>
+    </div>
   </div>
 )
 
