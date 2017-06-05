1f32046d0042750bd15858cd30c4dc2a4fe552cb
AliceKB <alicekb01@gmail.com@gmail.com>
2017-06-05 15:10:57 -0400

    Refactored Search component from 3 components to one.

--- src/components/meal_planner/Search.js
+++ src/components/meal_planner/Search.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
-import SearchBar from './Search/SearchBar';
-import SearchResult from './Search/SearchResult';
+
+import Meal from './Meal';
+
+const infoStyle = {
+  backgroundColor: 'white',
+  height: '93.5%',
+  padding: '1em',
+}
 
 export default class Search extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
-        <SearchResult />
+        <div className="ui fluid input">
+          <input type="text" placeholder="Search..." />
+          <button className='ui button'>Search</button>
+        </div>
+        <div className='ui column' style={infoStyle}>
+          <Meal name={'Sunday Supper: Jerk Half-Chickens'}/>
+          <Meal name={'Roasted Chickens With Lemon And Orange'}/>
+          <Meal name={'Sage Pesto Roasted Chicken'}/>
+          <Meal name={'Classic Roast Chicken Recipe'}/>
+          <Meal name={'Chicken Broth Elixir'}/>
+          <Meal name={'Mustard-Crusted Roast Chickens'}/>
+          <Meal name={'Sunday Supper: Jerk Half-Chickens'}/>
+          <Meal name={'Roasted Chickens With Lemon And Orange'}/>
+          <Meal name={'Sage Pesto Roasted Chicken'}/>
+          <Meal name={'Classic Roast Chicken Recipe'}/>
+          <Meal name={'Chicken Broth Elixir'}/>
+          <Meal name={'Mustard-Crusted Roast Chickens'}/>
+          <Meal name={'Sunday Supper: Jerk Half-Chickens'}/>
+          <Meal name={'Classic Roast Chicken Recipe'}/>
+        </div>
       </div>
     );
   }
