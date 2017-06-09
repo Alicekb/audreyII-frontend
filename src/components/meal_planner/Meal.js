9a2a611e6c6979e1f67731702ec8e0f4ecafdbee
AliceKB <alicekb01@gmail.com@gmail.com>
2017-06-09 15:13:49 -0400

    Transformed Meal to dragSource.

--- src/components/meal_planner/Meal.js
+++ src/components/meal_planner/Meal.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { DragSource } from 'react-dnd';
+import PropTypes from 'prop-types';
+
 import classNames from 'classnames';
+import { ItemTypes } from '../Types';
 
-const Meal = ({name, id, disabled, handleClick}) => (
-  <button 
-    className={ classNames('ui fluid button', {'disabled': disabled})}
-    onClick={handleClick}
-  >
-    {name}
-  </button>
-)
+const mealSource = {
+  beginDrag(props) {
+    return {
+      name: props.name,
+      id: props.id,
+      disabled: props.disabled
+    }
+  }
+}
 
-export default Meal;
+function collect(connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging()
+  }
+}
+
+const propTypes = {
+  name: PropTypes.string.isRequired,
+  isDragging: PropTypes.bool.isRequired,
+  connectDragSource: PropTypes.func.isRequired
+}
+
+class Meal extends Component {
+  render() {
+    const {name, id, disabled, handleClick, isDragging, connectDragSource } = this.props;
+    return connectDragSource(
+      <button 
+        className={ classNames('ui fluid button', {'disabled': disabled})}
+        onClick={handleClick}
+      >
+        {name}
+      </button>
+    )
+  }
+}
+
+Meal.propTypes = propTypes
+
+export default DragSource(ItemTypes.MEAL, mealSource, collect)(Meal)
