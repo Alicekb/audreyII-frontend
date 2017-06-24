986724ab20a8fdaac95ed7581228cca75036a2a6
AliceKB <alicekb01@gmail.com@gmail.com>
2017-06-24 14:41:38 -0400

    Updated Signup with semanti ui

--- src/components/Main/Forms/SignupForm.js
+++ src/components/Main/Forms/SignupForm.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
-import {Field, reduxForm} from 'redux-form';
-import RenderField from './RenderField.js';
-import validate from './Validate';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react'
+import {Field, reduxForm} from 'redux-form'
+import RenderField from './RenderField.js'
+import { Form, Button, Segment } from 'semantic-ui-react'
+import validate from './Validate'
+import PropTypes from 'prop-types'
 
 class SignupForm extends Component {
   static propTypes = { 
@@ -15,62 +16,51 @@ class SignupForm extends Component {
   render() {
     const {handleSubmit, pristine, reset, submitting, invalid} = this.props
     return (
-      <form
-        className='ui form fluid left aligned container'
+      <Form
         style={{padding: '2em', color: '#83B692'}}
         onSubmit={handleSubmit(this.handleSubmit.bind(this))}
       >
-        <div>
-          <label>Username</label>
-          <div>
-            <Field
-              name="username"
-              type="text"
-              component={RenderField}
-            />
-          </div>
-        </div>
+        <Form.Field>
+          <Field
+            label='Username:'
+            name="username"
+            type="text"
+            component={RenderField}
+          />
+        </Form.Field>
 
-        <div>
-          <label>Email</label>
-          <div>
-            <Field
-              name="email"
-              type="email"
-              component={RenderField}
-            />
-          </div>
-        </div>
+        <Form.Field>
+          <Field
+            label='Email:'
+            name="email"
+            type="email"
+            component={RenderField}
+          />
+        </Form.Field>
 
-        <div>
-          <label>Password</label>
-          <div>
-            <Field
-              name="password"
-              type="password"
-              component={RenderField}
-            />
-          </div>
-        </div>
+        <Form.Field>
+          <Field
+            label='Password:'
+            name="password"
+            type="password"
+            component={RenderField}
+          />
+          </Form.Field>
 
-        <div>
-          <label>Password Confirmation</label>
-          <div>
-            <Field
-              name="password_confirmation"
-              type="password"
-              component={RenderField}
-            />
-          </div>
-        </div>
-        
-        <div>
-          <button type="submit" disabled={pristine || submitting || invalid}>Submit</button>
-          <button type="button" disabled={pristine || submitting} onClick={reset}>
+        <Form.Field>
+          <Field
+            label='Password Confirmation:'
+            name="password_confirmation"
+            type="password"
+            component={RenderField}
+          />
+          </Form.Field>
+
+          <Button basic color='green' type="submit" disabled={pristine || submitting || invalid}>Submit</Button>
+          <Button basic type="button" type="button" disabled={pristine || submitting} onClick={reset}>
             Clear Values
-          </button>
-        </div>
-      </form>
+          </Button>
+      </Form>
     )
   }
 }
