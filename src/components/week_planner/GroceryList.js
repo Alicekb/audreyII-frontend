16f995bbbda27ea731d5041a55d4d8c8bd37f046
AliceKB <alicekb01@gmail.com@gmail.com>
2017-08-06 13:38:44 -0400

    Removed useless styling from GroceryList and Week components.

--- src/components/week_planner/GroceryList.js
+++ src/components/week_planner/GroceryList.js
@@ -1,29 +1,27 @@
-import React from 'react';
-import shortid from 'shortid';
-import {Grid, Header, List} from 'semantic-ui-react'
+import React from 'react'
+import shortid from 'shortid'
+import { Grid, Header, List } from 'semantic-ui-react'
 
-const GroceryList = ( { ingredients }) => (
-   <Grid.Row className='printing-grocery'>
-      <Grid stackable columns={5} style={{background: '#fff'}}>
-        <Header as='h2'><u>Groceries</u></Header>
-        { ingredients.map((ingredients, index) => {
-          return <List
-            celled
-            key={shortid.generate()}
-            className='column grocery-list'
-          >
-            { ingredients.map((ingredient)=> {
-                return <List.Item
-                  key={shortid.generate()}
-                  className='printing-li'
-                  >
-                    {ingredient}
+const GroceryList = ({ ingredients }) =>
+  <Grid.Row className="printing-grocery">
+    <Grid stackable columns={5} style={{ background: '#fff' }}>
+      <Header as="h2">
+        <u>Groceries</u>
+      </Header>
+      {ingredients.map((ingredients, index) => {
+        return (
+          <List celled key={shortid.generate()} className="column grocery-list">
+            {ingredients.map(ingredient => {
+              return (
+                <List.Item key={shortid.generate()} className="printing-li">
+                  {ingredient}
                 </List.Item>
+              )
             })}
           </List>
-        })}
-      </Grid>
+        )
+      })}
+    </Grid>
   </Grid.Row>
-)
 
 export default GroceryList
--- src/components/week_planner/Week.js
+++ src/components/week_planner/Week.js
@@ -1,72 +1,79 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router';
-import {Grid, Segment, Container, Button } from 'semantic-ui-react'
+import React, { Component } from 'react'
+import { withRouter } from 'react-router'
+import { Grid, Segment, Container, Button } from 'semantic-ui-react'
 
-import { connect } from 'react-redux';
-import { fetchWeek, resetWeek } from '../../actions/week';
-import shortid from 'shortid';
+import { connect } from 'react-redux'
+import { fetchWeek, resetWeek } from '../../actions/week'
+import shortid from 'shortid'
 
-import Date from '../Date';
-import GroceryList from './GroceryList';
-import Day from './Day';
+import Date from '../Date'
+import GroceryList from './GroceryList'
+import Day from './Day'
 
 class Week extends Component {
   componentDidMount() {
-    this.props.fetchWeek(this.props.location.state.id, 'http://localhost:3001/v1/weeks/', localStorage.getItem('token'))
+    this.props.fetchWeek(
+      this.props.location.state.id,
+      'http://localhost:3001/v1/weeks/',
+      localStorage.getItem('token')
+    )
   }
 
   componentWillUnmount() {
     this.props.resetWeek()
   }
 
-  chunkArray(array, chunk_size){
+  chunkArray(array, chunk_size) {
     let newArray = array.slice(0)
     let results = []
     while (newArray.length) {
-        results.push(newArray.splice(0, chunk_size));
+      results.push(newArray.splice(0, chunk_size))
     }
     return results
   }
 
   render() {
-    const days = this.props.daysArray.map((day) => {
-      return <Day 
-        key={shortid.generate()}
-        id={day.id}
-        name={day.day_name}
-      />
+    const days = this.props.daysArray.map(day => {
+      return <Day key={shortid.generate()} id={day.id} name={day.day_name} />
     })
     const ingredientsArray = this.chunkArray(this.props.ingredients, 5)
 
     return (
       <Container>
         <Segment>
-          <Grid textAlign='center'>
-            <Date week={this.props.location.state.id } year={this.props.location.state.year}/>
-            <Grid.Row className='printing-days'>
+          <Grid textAlign="center">
+            <Date
+              week={this.props.location.state.id}
+              year={this.props.location.state.year}
+            />
+            <Grid.Row className="printing-days">
               <Grid stackable columns={7}>
                 {days}
               </Grid>
             </Grid.Row>
-            <GroceryList ingredients={ingredientsArray}/>
+            <GroceryList ingredients={ingredientsArray} />
 
-            <Grid.Row columns={2} className='printing-display' style={{paddingTop: '0'}}>
+            <Grid.Row columns={2} className="printing-display">
               <Grid.Column>
-                <Button fluid color='green' onClick={() => window.print()}>Print Page</Button>
+                <Button fluid color="green" onClick={() => window.print()}>
+                  Print Page
+                </Button>
               </Grid.Column>
 
               <Grid.Column>
-                <Button fluid color='grey' onClick={this.props.history.goBack}>Back</Button>
+                <Button fluid color="grey" onClick={this.props.history.goBack}>
+                  Back
+                </Button>
               </Grid.Column>
             </Grid.Row>
           </Grid>
         </Segment>
       </Container>
-    );
+    )
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     daysArray: state.week.daysArray,
     meals: state.week.meals,
@@ -74,4 +81,6 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default withRouter(connect(mapStateToProps, { fetchWeek, resetWeek })(Week));
+export default withRouter(
+  connect(mapStateToProps, { fetchWeek, resetWeek })(Week)
+)
