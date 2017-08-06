1f0642acdc5cb5653a06d3ca33b52e628e81b22c
AliceKB <alicekb01@gmail.com@gmail.com>
2017-08-06 13:14:16 -0400

    Added padding-bottom to Calendar page background.

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,36 +1,41 @@
 import React, { Component } from 'react'
-import {Grid, Segment, Header, Container, Button } from 'semantic-ui-react'
+import { Grid, Segment, Header, Container, Button } from 'semantic-ui-react'
 import audreyApi from '../../api/AudreyApi'
 import shortid from 'shortid'
 
-import Date from '../Date';
-import { Redirect } from 'react-router-dom';
+import Date from '../Date'
+import { Redirect } from 'react-router-dom'
 
 export default class Calendar extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       year: '',
       weeks: [],
       redirect: false,
       weekId: 0
-    };
+    }
     this.handleClick = this.handleClick.bind(this)
   }
 
   componentDidMount() {
-    return audreyApi.getData(this.props.id, 'http://localhost:3001/v1/calendars/', localStorage.getItem('token'))
+    return audreyApi
+      .getData(
+        this.props.id,
+        'http://localhost:3001/v1/calendars/',
+        localStorage.getItem('token')
+      )
       .then(res => {
         if (res === undefined) {
           return null
         }
         this.getCalendar(res)
-    })
+      })
   }
 
   getCalendar(calendar) {
     let weeksArray = []
-    calendar.weeks.forEach((week) => {
+    calendar.weeks.forEach(week => {
       weeksArray.push(week.id)
     })
     this.setState({
@@ -39,7 +44,7 @@ export default class Calendar extends Component {
     })
   }
 
-  addWeek = (id) => {
+  addWeek = id => {
     let newWeek = this.state.weeks.slice()
     newWeek.push(id)
     this.setState({
@@ -55,44 +60,60 @@ export default class Calendar extends Component {
   }
 
   handleAddWeek = () => {
-    return audreyApi.createWeek(this.props.id, localStorage.getItem('token'))
+    return audreyApi
+      .createWeek(this.props.id, localStorage.getItem('token'))
       .then(res => {
         if (res === undefined) {
           return null
         }
         this.addWeek(res.id)
-    })
+      })
   }
 
   render() {
     if (!!this.state.redirect) {
-      return <Redirect push to={{
-        pathname: '/week',
-        state: {id: this.state.weekId , year: this.state.year}  
-      }}/>
+      return (
+        <Redirect
+          push
+          to={{
+            pathname: '/week',
+            state: { id: this.state.weekId, year: this.state.year }
+          }}
+        />
+      )
     }
 
-    const weeks = 
-      this.state.weeks.map((id, index) => {
-        return <Button
-          size='medium'
-          className='column'
-          style={{backgroundColor: '#fff', border: '1px solid #5D995D'}}
-          key={shortid.generate()} 
-          onClick={() => { this.handleClick(id)}}
+    const weeks = this.state.weeks.map((id, index) => {
+      return (
+        <Button
+          size="medium"
+          className="column"
+          style={{ backgroundColor: '#fff', border: '1px solid #5D995D' }}
+          key={shortid.generate()}
+          onClick={() => {
+            this.handleClick(id)
+          }}
         >
-          <Header as='h3'>
+          <Header as="h3">
             Week {index + 1}
           </Header>
         </Button>
-      })
+      )
+    })
 
     return (
       <Container>
-        <Segment textAlign='center'>
-          <Button compact floated='right' color='blue' onClick={this.handleAddWeek}>Add Week</Button>
-          <Date year={this.state.year} style={{marginBottom: '1em'}}/>
-          <Grid columns={5} textAlign='center' style={{marginTop: 0}}>
+        <Segment textAlign="center" style={{ paddingBottom: '3%' }}>
+          <Button
+            compact
+            floated="right"
+            color="blue"
+            onClick={this.handleAddWeek}
+          >
+            Add Week
+          </Button>
+          <Date year={this.state.year} style={{ marginBottom: '1em' }} />
+          <Grid columns={5} textAlign="center" style={{ marginTop: 0 }}>
             {weeks}
           </Grid>
         </Segment>
@@ -100,5 +121,3 @@ export default class Calendar extends Component {
     )
   }
 }
-
-
